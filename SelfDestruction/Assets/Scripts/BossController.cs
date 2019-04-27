753487fee2a5b9b72d52e63546476df7bc048ba1
Kirill Samoilenko <kirillsamoi@gmail.com>
2019-04-27 14:10:45 +0200

    attack and basic boss controller

---
+++ SelfDestruction/Assets/Scripts/BossController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossController : MonoBehaviour
+{
+
+    public Animator bossAnimator;
+
+
+    // Start is called before the first frame update
+    void GoLeft()
+    {
+        bossAnimator.SetTrigger("GoLeft");
+    }
+
+    void GoRight()
+    {
+        bossAnimator.SetTrigger("GoRight");
+    }
+
+
+}
