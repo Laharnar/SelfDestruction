b7bef18148fe9bf522970c8ec2741078077d0537
Kirill Samoilenko <kirillsamoi@gmail.com>
2019-04-27 16:25:36 +0200

    bullet adjusts

--- SelfDestruction/Assets/Scripts/Bullet.cs
+++ SelfDestruction/Assets/Scripts/Bullet.cs
@@ -1,12 +1,14 @@
 ﻿using UnityEngine;
 
 public class Bullet : MonoBehaviour {
+
+    public float bulletSpeed = 0.5f;
     private void Start() {
         Destroy(gameObject, 10);
     }
-    private void Update() {
 
-        transform.Translate(Vector2.up);
+    private void Update() {
+        transform.Translate(new Vector2(0, bulletSpeed));
 
     }
 }
