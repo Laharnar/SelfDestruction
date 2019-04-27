912a171c59d56ebc2887729acb091ce44a37b2ee
Kirill Samoilenko <kirillsamoi@gmail.com>
2019-04-27 21:12:20 +0200

    boss hp

---
+++ SelfDestruction/Assets/BossHPBarController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossHPBarController : MonoBehaviour
+{
+    public Image image;
+    public HealthController bosshp;
+    private float bossmaxHP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bossmaxHP = bosshp.health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        image.fillAmount = bosshp.health / bossmaxHP;
+    }
+}
