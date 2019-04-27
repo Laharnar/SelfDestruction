e14053daeceb673a1af52f9cde0c5537558c7525
Laharnar <marko.laharnar@gmail.com>
2019-04-27 18:39:20 +0200

    health controller

--- SelfDestruction/Assets/Scripts/HealthController.cs
+++ SelfDestruction/Assets/Scripts/HealthController.cs
@@ -1,7 +1,11 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class HealthController:MonoBehaviour {
     public int health=5;
+    public bool beInvulnerableOnHit = true;
+    public float onHitInvulnerabilityDuration = 1;
+    public Transform invulnerabilitySpriteTarget;// which sprite should dissapear when invulnerable
 
     private void OnCollisionEnter2D(Collision2D collision) {
         Bullet bullet = collision.transform.GetComponent<Bullet>();
@@ -14,6 +18,18 @@ public class HealthController:MonoBehaviour {
         health -= amount;
         if (health <= 0) {
             Destroy(gameObject);
+        } else if (beInvulnerableOnHit) { 
+            StartCoroutine(Invincibility());
         }
+    }
+
+    IEnumerator Invincibility() {
+        for (float i = 0; i < onHitInvulnerabilityDuration; i+=0.5f) {
+            invulnerabilitySpriteTarget.gameObject.SetActive(false);
+            yield return new WaitForSeconds(0.25f);
+            invulnerabilitySpriteTarget.gameObject.SetActive(true);
+            yield return new WaitForSeconds(0.25f);
+        }
+        yield return new WaitForSeconds(onHitInvulnerabilityDuration);
     }
 }
